9c74e99172a6b32be589c7d91e6c837d64321893
ByeonghwiJeong <byeonghwijeong@gmail.com>
2022-04-05 02:35:04 +0900

    Sequlize사용 - 테이블 생성, 데이터 추가, 테이터 조회

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const models = require("./models");
 const port = 8080;
 
 app.use(express.json());
@@ -9,42 +10,42 @@ app.use(cors());
 // 모든 브러우져에서 요청가능하게함
 
 app.get("/products", (req, res) => {
-  const query = req.query;
-  console.log("QUERY : ", query);
-  res.send({
-    products: [
-      {
-        id: 1,
-        name: "농구공",
-        price: 100000,
-        seller: "조던",
-        imageUrl: "images/products/basketball1.jpeg",
-      },
-      {
-        id: 2,
-        name: "축구공",
-        price: 50000,
-        seller: "메시",
-        imageUrl: "images/products/soccerball1.jpg",
-      },
-      {
-        id: 3,
-        name: "키보드",
-        price: 10000,
-        seller: "그랩",
-        imageUrl: "images/products/keyboard1.jpg",
-      },
-    ],
-  });
+  models.Product.findAll()
+    .then((result) => {
+      console.log("PRODUCTS : ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("에러발생");
+    });
 });
 
 app.post("/products", (req, res) => {
   const body = req.body;
-
-  res.send({
-    body,
-    // "body" : body ES6에서 key와 values가 같다면 body하나만 입력해도 무방함
-  });
+  const { name, description, price, seller } = body;
+  //======방어 코드 colunm값 하나라도 빠졌을때=======
+  if (!name || !description || !price || !seller) {
+    res.send("모든 필드를 입력해주세요!!");
+  }
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과 : ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품 업로드에 문제가 발생했습니다.");
+    });
 });
 
 app.get("/products/:id/event/:eventId", (req, res) => {
@@ -56,4 +57,14 @@ app.get("/products/:id/event/:eventId", (req, res) => {
 
 app.listen(port, () => {
   console.log("그랩의 쇼핑몰 서버가 돌아 가고 있습니다.");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공!!!");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("DB연결 실패");
+      process.exit();
+    });
 });
